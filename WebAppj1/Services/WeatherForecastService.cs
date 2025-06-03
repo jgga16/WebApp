a7502d691c8a290569f4de97569f8360dd44d3c1
jose garcia <jose.garcia@ec.gt.com>
2025-06-03 14:55:05 -0500

    datos uno

--- WebAppj1/Services/WeatherForecastService.cs
+++ WebAppj1/Services/WeatherForecastService.cs
@@ -9,7 +9,7 @@ namespace WebAppj1.Services
     {
         private static readonly string[] Summaries = new[]
         {
-            "uno-1", 
+            "uno", 
             "dos", 
             "tres", 
             "cuatro", 
